008b1f0f2b374fc202376ac6b6d282bda4c551fa
Kenta Kodashima <kentakodashima@gmail.com>
2018-10-24 22:25:30 -0700

    Implementing Home screen. Done with the following things: - Create custom UIView - Connect api with custom UIView and KolodaView.
    Things to do:
    - Refactor API's get function
    - Modified the CardView's UI
    - Implement Firebase anonymous authentication

---
+++ RecipeFlicker/Screens/Common/CardView.swift
@@ -0,0 +1,43 @@
+//
+//  CardView.swift
+//  RecipeFlicker
+//
+//  Created by Kenta Kodashima on 2018-10-23.
+//  Copyright © 2018 Kenta Kodashima. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CardView: UIView {
+  @IBOutlet weak var cardImage: UIImageView!
+  @IBOutlet weak var recipeTitle: UILabel!
+  
+  
+  /// For using custom view in code
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    commonInit()
+  }
+  
+  /// For using custom view in IB
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    commonInit()
+  }
+  
+  // Load xib and add it to the view
+  fileprivate func commonInit() {
+//    Bundle.main.loadNibNamed("CardView", owner: self, options: nil)
+    let bundle = Bundle(for: type(of: self))
+    let nib = UINib(nibName: "CardView", bundle: bundle)
+    let view = nib.instantiate(withOwner: self, options: nil).first as! UIView
+    addSubview(view)
+    
+    view.translatesAutoresizingMaskIntoConstraints = false
+    let bindings = ["view": view]
+    addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[view]|", options:NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: bindings))
+    addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[view]|", options:NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: bindings))
+  }
+  
+}
--- RecipeFlicker/Screens/Home/HomeVC.swift
+++ RecipeFlicker/Screens/Home/HomeVC.swift
@@ -7,12 +7,51 @@
 //
 
 import UIKit
+import Koloda
 
 class HomeVC: UIViewController {
 
+  @IBOutlet weak var kolodaView: KolodaView!
+  private var recipes = [Recipe]()
+  private var recipeAPI = RecipeAPI()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    kolodaView.dataSource = self
+    recipeAPI.getRandomRecipes { recipeArray, error in
+      for recipe in recipeArray! {
+        self.recipes.append(recipe)
+        self.kolodaView.reloadData()
+      }
+    }
   }
 }
 
+extension HomeVC: KolodaViewDataSource {
+  func kolodaNumberOfCards(_ koloda: KolodaView) -> Int {
+    return recipes.count
+  }
+  
+  func koloda(_ koloda: KolodaView, viewForCardAt index: Int) -> UIView {
+    let card = CardView(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
+    let recipe = recipes[index]
+    let imageUrl = URL(string: recipe.image)!
+    let session = URLSession(configuration: .default)
+    let downloadTask = session.dataTask(with: imageUrl) { (data, response, error) in
+      if let e = error {
+        print("Something went wrong.")
+      } else {
+        if let res = response as? HTTPURLResponse, let imageData = data {
+          card.cardImage.image = UIImage(data: imageData)
+        }
+      }
+    }
+    downloadTask.resume()
+    card.recipeTitle.text = recipe.title
+    
+    return card
+  }
+  
+  
+}
