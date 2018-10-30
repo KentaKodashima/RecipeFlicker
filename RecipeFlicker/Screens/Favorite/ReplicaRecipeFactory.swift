e7aea58fb244cf6b34cef5c18fc3c76774e04748
Minami Munakata <june28munazo@gmail.com>
2018-10-30 16:17:30 -0700

    create Replica Recipe Factory for testing the app

---
+++ RecipeFlicker/Screens/Favorite/ReplicaRecipeFactory.swift
@@ -0,0 +1,30 @@
+//
+//  ReplicaRecipeFactory.swift
+//  RecipeFlicker
+//
+//  Created by minami on 2018-10-30.
+//  Copyright © 2018 Kenta Kodashima. All rights reserved.
+//
+
+import Foundation
+
+class ReplicaRecipeFactory {
+  public var recipes: [ReplicaRecipe]
+  
+  init() {
+    recipes = [ReplicaRecipe]()
+    recipes = generateRecipes() + generateRecipes()
+  }
+  
+  func generateRecipes() -> [ReplicaRecipe] {
+    var newRecipes = [ReplicaRecipe]()
+    let category = ["Spanish", "Vietnameese", "Italian", "Japanese", "Chinese", "Korean"]
+    for i in 0..<6 {
+      newRecipes.append(ReplicaRecipe(
+        title: "Recipe Title",
+        image: "food" + String(i + 1),
+        whichCollectionToBelong: category[i] ))
+    }
+    return newRecipes
+  }
+}
