ab47eb921c490b9df4e78b4130f9411df0e7b046
Minami Munakata <june28munazo@gmail.com>
2018-10-31 08:15:22 -0700

    set up CollectionViewCellForCollection

---
+++ RecipeFlicker/Screens/Favorite/CollectionViewCellForAll.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCellForAll.swift
+//  RecipeFlicker
+//
+//  Created by minami on 2018-10-30.
+//  Copyright © 2018 Kenta Kodashima. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCellForAll: UICollectionViewCell {
+    
+}
