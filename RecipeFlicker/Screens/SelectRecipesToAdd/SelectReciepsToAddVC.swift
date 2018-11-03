6f85013756b9988190b6b44b4df833a25c19eacc
Kenta Kodashima <kentakodashima@gmail.com>
2018-11-03 11:05:30 -0700

    Add screens and VCs to add new collections.

---
+++ RecipeFlicker/Screens/SelectRecipesToAdd/SelectReciepsToAddVC.swift
@@ -0,0 +1,30 @@
+//
+//  SelectReciepsToAddVC.swift
+//  RecipeFlicker
+//
+//  Created by Kenta Kodashima on 2018-11-03.
+//  Copyright © 2018 Kenta Kodashima. All rights reserved.
+//
+
+import UIKit
+
+class SelectReciepsToAddVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
