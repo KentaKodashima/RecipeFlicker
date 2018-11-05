c1cb73c95aed2805675ed7c39ded0031efe2dc37
Kenta Kodashima <kentakodashima@gmail.com>
2018-11-04 23:54:27 -0800

    Implementing the following things:  - AddCollection screen  - SelectRecipesToAdd screen  - Collection model  - Firebase architecture for Collection model
    Add:
     - Model/Collection.swift
    
    Modified:
     - Files below Screens/AddCollection/
     - Files below Screens/SelectRecipesToAdd/
     - Change the directory:
        - Screens/Common/Cell/CollectionViewCellForGrid.swift
        - Screens/Common/Cell/RecipeListTableViewCell.swift
    
    ToDo:
     - SelectRecipesToAddVC: Make Collection out of selected tableView rows
     - SelectRecipesToAddVC: Back to FavoriteVC and update the UI

--- RecipeFlicker/Screens/Detail/DetailVC.swift
+++ RecipeFlicker/Screens/Detail/DetailVC.swift
@@ -2,7 +2,7 @@
 //  DetailVC.swift
 //  RecipeFlicker
 //
-//  Created by Kenta Kodashima on 2018-10-16.
+//  Created by minami on 2018-10-16.
 //  Copyright © 2018 Kenta Kodashima. All rights reserved.
 //
 
