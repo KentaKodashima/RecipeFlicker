3259c03853c5b34dac9ba576b95e6258ccfb0091
Minami Munakata <june28munazo@gmail.com>
2018-12-20 17:39:12 -0800

    comment 'deleteRecipeFromCollection'

--- RecipeFlicker/Model/Collection.swift
+++ RecipeFlicker/Model/Collection.swift
@@ -54,10 +54,10 @@ extension Collection {
     Database.database().reference(withPath: recipeCollectionsRefPath)
       .child(recipe.firebaseId).setValue(recipe.convertToJSON())
   }
-  
-  func deleteRecipeFromCollection(recipe: Recipe) {
-    let ref = Database.database().reference()
-    ref.child("recipeCollections/\(self.firebaseId)/\(recipe.firebaseId)").removeValue()
-
-  }
+//
+//  func deleteRecipeFromCollection(recipe: Recipe) {
+//    let ref = Database.database().reference()
+//    ref.child("recipeCollections/\(self.firebaseId)/\(recipe.firebaseId)").removeValue()
+//
+//  }
 }
