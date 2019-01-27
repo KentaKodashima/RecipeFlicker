95465595730b332ed0b085ce2569679fff72f9c2
Kenta Kodashima <kentakodashima@gmail.com>
2019-01-26 18:47:02 -0800

    Implementing the new countdownView design.
    TODO:
     - Implement UICollectionView programmatically
     - Implement UIPageControl programatically
     - Adjsut the countdownView's UI

---
+++ RecipeFlicker/Screens/Common/Cell/RecommendationCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  RecommendationCollectionViewCell.swift
+//  RecipeFlicker
+//
+//  Created by Kenta Kodashima on 2019-01-26.
+//  Copyright © 2019 Kenta Kodashima. All rights reserved.
+//
+
+import UIKit
+
+class RecommendationCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
